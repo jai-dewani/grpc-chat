8d513a7312f7cc3d1b3ed2a4f28d2d21c12e4050
Jai <jai.dewani.99@gmail.com>
2021-10-03 19:42:08 +0530

    Change from to source to avoid conflict with identifiers in python

--- ChatAppClient/Program.cs
+++ ChatAppClient/Program.cs
@@ -16,22 +16,28 @@ namespace ChatAppClient
             AsyncDuplexStreamingCall<Chat,Chat> stream = client.ChatStream();
             Console.Write("What's your name?: ");
             string Name = Console.ReadLine();
-
+            
             var readResponses = Task.Run(async () =>
             {
                 while (await stream.ResponseStream.MoveNext())
                 {
-                    System.Console.WriteLine("MESSAGE");
+                    int top = Console.CursorTop;
                     var message = stream.ResponseStream.Current;
-                    Console.WriteLine($"{message.From}: {message.Message}");
+                    Console.SetCursorPosition(0,top);
+                    Console.WriteLine($"{message.Source}: {message.Message}");
+                    Console.Write($"{Name}: ");
                 };
                 System.Console.WriteLine("DONE");
             });
 
             while (true)
             {
+                Console.Write($"{Name}: ");
                 var message = Console.ReadLine();
-                stream.RequestStream.WriteAsync(new Chat { From = Name, Message = message });
+                int top = Console.CursorTop;
+                int left = Console.CursorLeft;
+                Console.SetCursorPosition(left,top-1);
+                stream.RequestStream.WriteAsync(new Chat { Source = Name, Message = message });
             };
         }
     }
